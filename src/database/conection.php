a1e5048954d4da36d0635948aa837f2fe1b9a91f
pedro <ph8464803@gmail.com>
2020-11-20 15:24:54 -0300

    frist

--- src/database/conection.php
+++ src/database/conection.php
@@ -1,15 +1,13 @@
 <?php
 
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'password');
+define('MYSQL_DB_NAME', 'todoList');
 
-
-use PDO;
-
-class Conection
-{
-    public static function creatConnection(): \PDO
-    {
-        $databasePath = __DIR__ . '/../../../banco.sqlite';
-        return new PDO('sqlite:' . $databasePath);
-    }
-
+try {
+    $PDO = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
+} catch (PDOException $e) {
+    echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
 }
+
